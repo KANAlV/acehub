27744b0d7c3694f05ce3abd3676514cb7e0b59aa
KANAlV <editicroyedics@gmail.com>
2026-04-11 15:13:29 +0800

    fix(auth): Add navigateToLoginRequestUrl option to auth config

--- app/authConfig.ts
+++ app/authConfig.ts
@@ -3,6 +3,7 @@ export const msalconfig = {
         clientId: "40bafd50-ad27-4b77-a8cd-0b43e911ee82",
         authority: "https://login.microsoftonline.com/organizations",
         redirectUri: process.env.NEXT_PUBLIC_REDIRECT_URI || "http://localhost:3000/auth-callback",
+        navigateToLoginRequestUrl: false,
     },
     cache: {
         cacheLocation: "localStorage", 
