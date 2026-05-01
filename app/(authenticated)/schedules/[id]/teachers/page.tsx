c5019e073bd39b14354f0a2b5dc550e478aa3ce6
kanaiv <editicroyedics@gmail.com>
2026-05-01 17:06:23 +0800

    fix: changed useRouter to redirect ([id]/teachers)

--- app/(authenticated)/schedules/[id]/teachers/page.tsx
+++ app/(authenticated)/schedules/[id]/teachers/page.tsx
@@ -1,8 +1,6 @@
-import {useRouter} from "next/navigation";
 import {use} from "react";
 
 export default function ScheduleSummary({ params }: { params: Promise<{ id: string }> }) {
-    const router = useRouter();
     const {id} = use(params);
 
 }
